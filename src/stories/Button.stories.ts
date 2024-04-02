7c1ebd3056d63bc0c8653d2f1f8e5757d8bd7258
unknown <wknowpark797@gmail.com>
2024-04-02 21:35:30 +0900

    Button 컴포넌트 스토리 분석 및 구조 수정

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,52 +1,47 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from './Button';
+import { Button } from '../components/Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// meta: Button 컴포넌트의 메타 정보
+// Button 컴포넌트의 props를 정의
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+	title: 'Basic/Button', // 경로
+	component: Button, // 스토리의 컴포넌트
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+
+	argTypes: {
+		primary: { control: 'boolean', description: '버튼의 타입' },
+		backgroundColor: { control: 'color', description: '버튼의 배경컬러' },
+	},
+	args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
+	args: {
+		primary: true,
+		label: 'Button',
+	},
 };
-
 export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
+	args: {
+		label: 'Button',
+	},
 };
-
 export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
+	args: {
+		size: 'large',
+		label: 'Button',
+	},
 };
-
 export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+	args: {
+		size: 'small',
+		label: 'Button',
+	},
 };
