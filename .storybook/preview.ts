9b063b58c1968beec598bdbeb9720fa46f843449
unknown <wknowpark797@gmail.com>
2024-04-02 22:26:20 +0900

    Label 컴포넌트 및 스토리 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,17 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import '../src/index.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
